29f08f84dca0dc2727faeb6f36ff8cc897d6f65d
Aleksey Popov <alepooop@gmail.com>
2020-02-28 14:07:36 +0200

    :white_check_mark: Fix tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,42 +2,34 @@ import { signTransaction, getPublicKey } from './index';
 import { prepareTransaction } from './sign';
 import { derivePath } from './utils';
 import * as eded25519 from 'ed25519-hd-key';
+import { TransferTransaction } from '@liskhq/lisk-transactions'
 
 const path = "m/44'/134'/0'/0'/0'";
 const hexSeed = '06bae687f0250ab9533be2ac9717ae2a802d69c97d547c2e862e66e05453b165cf3e06a649c826a6db3b702644dc1c9154ad3b4188b8c15848d83a87c1c48eca';
-const unsignedTransaction = {
-    "amount": "100000000000",
-    "recipientId": "666L",
-    "timestamp": 60225629,
-    "type": 0,
-    "fee": "20000000",
-    "asset": {
-        "data": "lol wat?"
-    }
-};
+const networkIdentifier = 'e48feb88db5b5cf5ad71d93cdcd1d879b6d5ed187a36b0002cc34e0ef9883255';
+const tx = new TransferTransaction({ networkIdentifier });
 
 describe('Main module', () => {
     describe('signTransaction', () => {
         beforeEach(() => {
-            (prepareTransaction as any) = jest.fn();
+            prepareTransaction = jest.fn(() => tx);
         });
         afterEach(() => {
-            (prepareTransaction as any).mockRestore();
+            prepareTransaction.mockRestore();
         });
 
         it('should add senderPublicKey field to transaction if it not exist', () => {
-            (prepareTransaction as any).mockReturnValue(unsignedTransaction);
-            const signedTransaction = signTransaction(hexSeed, path, unsignedTransaction);
+            const signedTransaction = signTransaction(hexSeed, path, tx);
             expect(signedTransaction.senderPublicKey).toBeDefined();
         });
 
         it('should convert ed25519 32 byte -> NaCL 64 byte private key', () => {
             const { key } = derivePath(path, hexSeed);
             const publicKey = eded25519.getPublicKey(key, false);
-            signTransaction(hexSeed, path, unsignedTransaction);
+            signTransaction(hexSeed, path, tx);
             expect(prepareTransaction).toBeCalledWith(
-                Object.assign(unsignedTransaction, { senderPublicKey: publicKey.toString('hex')}),
-                [...key, ...publicKey]
+                Object.assign(tx, { _senderPublicKey: publicKey.toString('hex')}),
+                Buffer.concat([key, publicKey])
             );
         });
     });
--- src/sign/index.spec.ts
+++ src/sign/index.spec.ts
@@ -1,53 +1,51 @@
-jest.mock('lisk-elements');
-import elements from 'lisk-elements';
-const {
-    cryptography,
-    transaction: { utils },
-} = elements;
-
+import * as cryptography from '@liskhq/lisk-cryptography'
+import { BaseTransaction, utils, TransferTransaction } from '@liskhq/lisk-transactions'
 import { signTransaction, prepareTransaction } from './';
 
 const transactionHash = '0x8';
 const privateKey = Buffer.from('privateKey');
+const networkIdentifier = 'e48feb88db5b5cf5ad71d93cdcd1d879b6d5ed187a36b0002cc34e0ef9883255';
+const tx = new TransferTransaction({ networkIdentifier, senderPublicKey: 'efaf1d977897cb60d7db9d30e8fd668dee070ac0db1fb8d184c06152a8b75f8d' });
 
 describe('Sign', () => {
     describe('signTransaction', () => {
         beforeAll(() => {
-            utils.getTransactionHash.mockImplementation(() => transactionHash)
+            cryptography.hash = jest.fn(() => transactionHash)
+            cryptography.signDataWithPrivateKey = jest.fn();
         })
-        it('should call lisk-elements getTransactionHash method', () => {
-            signTransaction({}, privateKey);
-            expect(utils.getTransactionHash).toBeCalled();
+        it('should call lisk-cryptography hash method', () => {
+            signTransaction(tx, privateKey);
+            expect(cryptography.hash).toBeCalled();
         });
 
-        it('should call lisk-elements signDataWithPrivateKey', () => {
-            signTransaction({}, privateKey);
+        it('should call lisk-cryptography signDataWithPrivateKey', () => {
+            signTransaction(tx, privateKey);
             expect(cryptography.signDataWithPrivateKey).toBeCalledWith(transactionHash, privateKey)
         });
     });
 
     describe('prepareTransaction', () => {
-        const signature = '123';
+        const signature = '9fc2b85879b6423893841343c1d8905f3b9118b7db96bbb589df771c35ce0d05ce446951ee827c76ed1a85951af40018a007a1663f1a43a50129a0e32f26cb03';
         const id = '123';
         beforeAll(() => {
             (signTransaction as any) = jest.fn(() => signature);
-            utils.getTransactionId.mockImplementation(() => id);
+            utils.getId = jest.fn(() => id);
         });
         afterAll(() => {
             (signTransaction as any).mockRestore();
         })
         it('should call signTransaction', () => {
-            prepareTransaction({}, privateKey);
-            expect(signTransaction).toBeCalledWith({}, privateKey);
+            prepareTransaction(tx, privateKey);
+            expect(signTransaction).toBeCalledWith(tx, privateKey);
         });
 
-        it('should call lisk-elements getTransactionId method', () => {
-            prepareTransaction({}, privateKey);
-            expect(utils.getTransactionId).toBeCalledWith({ signature });
+        it('should call lisk-cryptography getId method', () => {
+            prepareTransaction(tx, privateKey);
+            expect(utils.getId).toBeCalledWith(tx.getBytes());
         });
 
         it('should return transaction with signature and id fields', () => {
-            expect(prepareTransaction({}, privateKey)).toEqual({id, signature })
+            expect(prepareTransaction(tx, privateKey)).toEqual(Object.assign(tx, {_signature: signature, _id: id }));
         })
     });
 });
